887331e603cead6c1678fc0b85becfc4ac8b1442
krsnmvk <krsnmvk@gmail.com>
2025-09-22 13:37:48 +0000

    03: implementing the game logic

--- 02-dom-and-events/01-guess-my-number/script.js
+++ 02-dom-and-events/01-guess-my-number/script.js
@@ -7,12 +7,43 @@ document.querySelector('.score').textContent = 15;
 
 console.log((document.querySelector('.guess').value = 20));
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = parseInt(document.querySelector('.guess').value);
 
-  console.log(guess, typeof guess);
+  console.log(guess, typeof guess, secretNumber);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number';
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+
+      // score = score -1
+      score--;
+
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+
+      // score = score -1
+      score--;
+
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });

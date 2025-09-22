3d96f7de4331e75295508cc0e9c252a71380b955
krsnmvk <krsnmvk@gmail.com>
2025-09-22 12:04:00 +0000

    01: selecting and manipulating elements

--- 02-dom-and-events/01-guess-my-number/script.js
+++ 02-dom-and-events/01-guess-my-number/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+console.log(document.querySelector('.message'));
+
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 15;
+
+console.log((document.querySelector('.guess').value = 20));

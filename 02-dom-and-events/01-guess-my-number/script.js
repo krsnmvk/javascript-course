ca4bc541154fe28d926fa8eaf7a36f0148da9f0b
krsnmvk <krsnmvk@gmail.com>
2025-09-22 12:59:32 +0000

    02: handling click events

--- 02-dom-and-events/01-guess-my-number/script.js
+++ 02-dom-and-events/01-guess-my-number/script.js
@@ -1,8 +1,18 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message'));
 
 document.querySelector('.number').textContent = 10;
 document.querySelector('.score').textContent = 15;
 
 console.log((document.querySelector('.guess').value = 20));
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = parseInt(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+  }
+});

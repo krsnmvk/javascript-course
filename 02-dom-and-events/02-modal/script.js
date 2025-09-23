ddb98aaadc12c004511094859676ee91befc0444
krsnmvk <krsnmvk@gmail.com>
2025-09-23 10:00:22 +0700

    01: working with classes

--- 02-dom-and-events/02-modal/script.js
+++ 02-dom-and-events/02-modal/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+
+  btnShowModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+
+    console.log(modal.classList);
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);

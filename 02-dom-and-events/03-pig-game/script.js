02cabae971ab5271ac7f7fba2089c098c70cb5bf
krsnmvk <krsnmvk@gmail.com>
2025-09-23 13:00:02 +0700

    02: switching the active player

--- 02-dom-and-events/03-pig-game/script.js
+++ 02-dom-and-events/03-pig-game/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -9,12 +11,14 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-score0El.textContent = 5;
-score1El.textContent = 10;
+score0El.textContent = 0;
+score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -32,6 +36,16 @@ btnRoll.addEventListener('click', function () {
 
     console.log(currentScore);
 
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+    currentScore = 0;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });

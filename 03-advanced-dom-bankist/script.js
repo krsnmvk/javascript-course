e8b6c0761bb435ac58f8e0a98132728b64f25d2e
krsnmvk <krsnmvk@gmail.com>
2025-09-30 07:59:06 +0700

    02: implementing smooth scrolling

--- 03-advanced-dom-bankist/script.js
+++ 03-advanced-dom-bankist/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnsScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -28,4 +30,8 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+btnsScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
 });

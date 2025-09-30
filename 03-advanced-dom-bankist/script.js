0bd7b01b716eca86317003b084c267782bf9889c
krsnmvk <krsnmvk@gmail.com>
2025-09-30 08:15:24 +0700

    03: event delegation - implementing page navigation

--- 03-advanced-dom-bankist/script.js
+++ 03-advanced-dom-bankist/script.js
@@ -35,3 +35,12 @@ document.addEventListener('keydown', function (e) {
 btnsScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
